b6a96cc2e02a73d84b0f0975b968ef70a727cb31
Felipe Martin <me@fmartingr.com>
2019-12-14 21:55:30 +0100

    Some code improvements:
    - Better handling game images using a named tuple with defined
    attributes.
    - Extract the timestamp/game id using slicing
    - Write not found Game IDs only (sorted to ease out filling the
    game_ids.json)

--- organize_screenshots.py
+++ organize_screenshots.py
@@ -5,20 +5,30 @@
 # View README.md for more details.
 # More information: https://github.com/RenanGreca/Switch-Screenshots
 
-import os
-import json
-from shutil import copy2
 import argparse
+import json
+import os
 import sys
+from collections import namedtuple
+from shutil import copy2
+
+
+# List of allowed extensions for files
+ALLOWED_EXTENSIONS = [".jpg", ".jpeg", ".png", ".mp4"]
+TIMESTAMP_LEN = 16
+GAME_ID_LEN = 32
 
 # Argument parser
-parser = argparse.ArgumentParser(description='''Nintendo Switch screenshot organizer. 
-                    Identifies images and creates new directory structure based on game title. 
+parser = argparse.ArgumentParser(description='''Nintendo Switch screenshot organizer.
+                    Identifies images and creates new directory structure based on game title.
                     More information: https://github.com/RenanGreca/Switch-Screenshots''')
 parser.add_argument('-i', '--input_dir', type=str, required=False, default='./Album/',
                     help='(str) Path to the Album directory. Default: ./Album/')
 parser.add_argument('-o', '--output_dir', type=str, required=False, default='./Output/',
                     help='(str) Desired output directory. Default: ./Output/')
+
+
+GameImage = namedtuple("GameImage", ("path", "timestamp", "game_id", "extension"))
 
 # Create a list of all the image/video files in the Album directory.
 # Thanks to L. Teder
@@ -27,40 +37,43 @@ def list_images(dir):
     r = []
     for root, _, files in os.walk(dir):
         for name in files:
-            if "jpg" in name or "mp4" in name or "png" in name:
-                r.append(os.path.join(root, name))
+            path = os.path.join(root, name)
+            _, extension = os.path.splitext(path)
+            if extension in ALLOWED_EXTENSIONS:
+                timestamp = name[0:TIMESTAMP_LEN]
+                game_id= name[TIMESTAMP_LEN + 1:TIMESTAMP_LEN + GAME_ID_LEN + 1]
+                game_image = GameImage(path=path, timestamp=timestamp, extension=extension, game_id=game_id)
+                r.append(game_image)
     return r
+
 
 def organize_screenshots(game_ids, input_dir, output_dir):
     images = list_images(input_dir)
     count = len(images)
 
-    not_found = []
+    not_found = set()
     # Iterate over images
     for idx, image in enumerate(images):
-        # Split the filename to find the game ID
-        image_id = image.split('-')[1].split('.')[0]
-        
-        folder_name = image_id
-        if image_id in game_ids:
+        folder_name = image.game_id
+        if image.game_id in game_ids:
             # If the ID was in the JSON file, the directory is named with the title
-            folder_name = game_ids[image_id]
+            folder_name = game_ids[image.game_id]
         else:
-            not_found.append(image)
+            not_found.add(image.game_id)
 
         # Create the directory and copy the file
         path = os.path.join(output_dir, folder_name)
         if not os.path.exists(path):
             os.makedirs(path)
-        copy2(image, path)
+        copy2(image.path, path)
 
         # Print progress indicator
         sys.stdout.write("\r"+str(idx+1)+"/"+str(count))
         sys.stdout.flush()
 
     if len(not_found):
-        print("\nGame IDs not found for the following files:")
-        print("\n".join(not_found))
+        print("\nNames not found for the following game IDs:")
+        print("\n".join(sorted(not_found)))
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -70,5 +83,5 @@ if __name__ == '__main__':
     # Load game IDs file
     with open('game_ids.json') as data_file:
         game_ids = json.load(data_file)
-    
+
     organize_screenshots(game_ids, input_dir, output_dir)
